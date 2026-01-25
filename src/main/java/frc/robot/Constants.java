7aa0db615e03e87f60c96c9a52baa0a2cfc04cc1
dev <arnavakashgupta@gmail.com>
2026-01-24 18:13:26 -0600

    Removed empty file
    We had previously accidently imported an empty java file to an incorrect part of our code at some point.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
         public static final int minTags = 2; //*Should always be at least 1*, decide between 1 and 2 in most cases. This year I think 2 is better because the tags usually comes in pairs of two, so it is ok to ignore singular tags
         public static final double maxYawRate_DegPerSec = 500; //ignore visual measurements when yaw rate is greater than this value
         public static final double maxTagDistance_Meters = 5.0; //ignore visual measurements when tag distance is greater than this value
-        public static final double minStdDev_Meters = 0.01; //std dev for vision measurement is always at least this value regardless of distance
+        public static final double minStdDev_Meters = 0.01; //std dev for xy vision measurement can never be smaller than this value
         public static final double stdDevPerMeter = 0.02; //Std dev increase per meter; farther from tag means less accurate measurement
     }
 
